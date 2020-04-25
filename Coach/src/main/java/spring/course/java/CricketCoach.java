c3f8e19a87c2bc5f7518342a91b02c125045556a
katemalets <katyamask@gmail.com>
2020-04-25 21:23:43 +0300

    literal values

--- Coach/src/main/java/spring/course/java/CricketCoach.java
+++ Coach/src/main/java/spring/course/java/CricketCoach.java
@@ -4,6 +4,28 @@ public class CricketCoach implements Coach {
 
     private FortuneService fortuneService;
 
+    private String email;
+
+    private String team;
+
+    public void setEmail(String email) {
+        System.out.println("setEmail works");
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        System.out.println("setTeam works");
+        this.team = team;
+    }
+
     public CricketCoach(){
         System.out.println("Cricket coach constructor works");
     }
--- Coach/src/main/java/spring/course/java/DemoAppSetter.java
+++ Coach/src/main/java/spring/course/java/DemoAppSetter.java
@@ -6,9 +6,11 @@ public class DemoAppSetter {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext.xml");
-        Coach coach = context.getBean("cricketCoach", Coach.class);
+        CricketCoach coach = context.getBean("cricketCoach", CricketCoach.class);
         System.out.println(coach.getDailyWorkOut());
         System.out.println(coach.getFortune());
+        System.out.println(coach.getEmail());
+        System.out.println(coach.getTeam());
         context.close();
     }
 }

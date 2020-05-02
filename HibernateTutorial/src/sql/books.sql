500f5faa7960fa36c166b30fe40b5b5b7694be07
katemalets <katyamask@gmail.com>
2020-05-02 19:09:26 +0300

    counting books sql

---
+++ HibernateTutorial/src/sql/books.sql
@@ -0,0 +1,7 @@
+INSERT INTO books.book (id, name, author)
+    VALUES (1, 'Effective Java', 'Joshua Bloch');
+INSERT INTO books.book (id, name, author)
+    VALUES (2, 'Java Concurrency in Practice', 'Brian Goetz');
+
+
+    

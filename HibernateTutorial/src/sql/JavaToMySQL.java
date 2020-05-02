6954b737462d9ddc587b63ba46feb917d0aea279
katemalets <katyamask@gmail.com>
2020-05-02 19:05:21 +0300

    counting books sql

---
+++ HibernateTutorial/src/sql/JavaToMySQL.java
@@ -0,0 +1,51 @@
+package sql;
+
+import java.sql.*;
+
+public class JavaToMySQL {
+    // JDBC URL, username and password of MySQL server
+    private static final String url = "jdbc:mysql://127.0.0.1:3306/?user=hbstudent"+
+            "&serverTimezone=UTC";
+    private static final String user = "hbstudent";
+    private static final String password = "hbstudent";
+
+    // JDBC variables for opening and managing connection
+    private static Connection con;
+    private static Statement stmt;
+    private static ResultSet rs;
+
+    public static void main(String[] args) {
+
+        String query = "select count(*) from books.book";
+
+        try {
+            // opening database connection to MySQL server
+            con = DriverManager.getConnection(url, user, password);
+
+            // getting Statement object to execute query
+            stmt = con.createStatement();
+
+            // executing SELECT query
+            rs = stmt.executeQuery(query);
+
+            while (rs.next()) {
+                int count = rs.getInt(1);
+                System.out.println("Total number of books in the table : " + count);
+            }
+
+        } catch (SQLException sqlEx) {
+            sqlEx.printStackTrace();
+        } finally {
+            //close connection ,stmt and resultset here
+            try {
+                con.close();
+            } catch (SQLException se) { /*can't do anything */ }
+            try {
+                stmt.close();
+            } catch (SQLException se) { /*can't do anything */ }
+            try {
+                rs.close();
+            } catch (SQLException se) { /*can't do anything */ }
+        }
+    }
+}

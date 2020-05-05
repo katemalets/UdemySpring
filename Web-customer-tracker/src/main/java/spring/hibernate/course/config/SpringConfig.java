7b223a29e1427d3348923bd015add24549bbcbe5
katemalets <katyamask@gmail.com>
2020-05-05 18:21:09 +0300

    connecting to db (on server)

---
+++ Web-customer-tracker/src/main/java/spring/hibernate/course/config/SpringConfig.java
@@ -0,0 +1,26 @@
+package spring.hibernate.course.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan("spring.hibernate.course")
+@EnableWebMvc
+public class SpringConfig implements WebMvcConfigurer {
+
+    @Bean
+    public ViewResolver viewResolver() {
+
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+
+        return viewResolver;
+    }
+}

a94c420574cf3d44189f3c27874493f0c97a216f
katemalets <katyamask@gmail.com>
2020-05-20 15:54:46 +0300

    create @PointCut declaration

--- SpringAOP/src/main/java/spring/course/java/aspect/LoggingAspect.java
+++ SpringAOP/src/main/java/spring/course/java/aspect/LoggingAspect.java
@@ -2,13 +2,17 @@ package spring.course.java.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LoggingAspect {
 
-    @Before("execution(* spring.course.java.dao.*.*(..))")
+    @Pointcut("execution(* spring.course.java.dao.*.*(..))")
+    private void daoPackage(){}
+
+    @Before("daoPackage()")
     public void beforeAddAccount(){
         System.out.println("BEFORE annotation works");
     }

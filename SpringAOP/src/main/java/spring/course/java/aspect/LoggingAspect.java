a44aec9acfe2ea9a34aafc81629231236644fc59
katemalets <katyamask@gmail.com>
2020-05-20 12:54:09 +0300

    execution changes(full name)

--- SpringAOP/src/main/java/spring/course/java/aspect/LoggingAspect.java
+++ SpringAOP/src/main/java/spring/course/java/aspect/LoggingAspect.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class LoggingAspect {
 
-    @Before("execution(public void addAccount())")
+    @Before("execution(public void spring.course.java.dao.AccountDAO.addAccount())")
     public void beforeAddAccount(){
         System.out.println("BEFORE annotation works");
     }

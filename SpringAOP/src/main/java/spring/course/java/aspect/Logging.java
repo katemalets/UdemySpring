e32390b8b1de63a7e0d8dc79b765e7ceb5146f22
katemalets <katyamask@gmail.com>
2020-05-22 13:57:17 +0300

    @Around handle exception

--- SpringAOP/src/main/java/spring/course/java/aspect/Logging.java
+++ SpringAOP/src/main/java/spring/course/java/aspect/Logging.java
@@ -19,7 +19,13 @@ public class Logging {
     public Object around(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
         System.out.println("---around: " + proceedingJoinPoint.getSignature().toShortString());
         long begin = System.currentTimeMillis();
-        Object result = proceedingJoinPoint.proceed();
+        Object result = null;
+        try {
+            result = proceedingJoinPoint.proceed();
+        } catch (Exception exc) {
+            System.out.println("---mistake is caught");
+            result = "AOP corrected your mistake, yeeep";
+        }
         long end = System.currentTimeMillis();
         System.out.println("Destination: " + (end - begin)/1000 + " seconds");
         return result;

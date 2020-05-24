453eea813e246df1f8bd7273ad16231e63597c39
katemalets <katyamask@gmail.com>
2020-05-24 10:40:20 +0300

    user accounts stored in db

--- SpringSecurityDemo/src/main/java/spring/security/course/config/DemoAppConfig.java
+++ SpringSecurityDemo/src/main/java/spring/security/course/config/DemoAppConfig.java
@@ -1,16 +1,30 @@
 package spring.security.course.config;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+import java.util.logging.Logger;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "spring.security.course")
+@PropertySource("classpath:persistence-mysql.properties")
 public class DemoAppConfig {
+
+    @Autowired
+    private Environment environment;
+
+    private Logger logger = Logger.getLogger(getClass().getName());
 
     @Bean
     public ViewResolver viewResolver(){
@@ -20,5 +34,36 @@ public class DemoAppConfig {
         internalResourceViewResolver.setPrefix("/WEB-INF/views/");
         internalResourceViewResolver.setSuffix(".jsp");
         return internalResourceViewResolver;
+    }
+
+    @Bean
+    public DataSource securityDataSource(){
+        ComboPooledDataSource securityDataSource =
+                new ComboPooledDataSource();
+
+        try {
+            securityDataSource.setDriverClass(environment.getProperty("jdbc.driver"));
+        } catch (PropertyVetoException exc) {
+            throw new RuntimeException("Error message: " + exc);
+        }
+
+        securityDataSource.setJdbcUrl(environment.getProperty("jdbc.url"));
+        securityDataSource.setUser(environment.getProperty("jdbc.user"));
+        securityDataSource.setPassword(environment.getProperty("jdbc.password"));
+
+        logger.info("\n\n>>>>>>>>>Driver: " + environment.getProperty("jdbc.driver") + "\n");
+        logger.info("\n>>>>>>>>>Url: " + environment.getProperty("jdbc.url") + "\n");
+        logger.info("\n>>>>>>>>>User: " + environment.getProperty("jdbc.user") + "\n");
+
+        securityDataSource.setInitialPoolSize(getIntProperty("connection.pool.initialPoolSize"));
+        securityDataSource.setMinPoolSize(getIntProperty("connection.pool.minPoolSize"));
+        securityDataSource.setMaxPoolSize(getIntProperty("connection.pool.maxPoolSize"));
+        securityDataSource.setMaxIdleTime(getIntProperty("connection.pool.maxIdleTime"));
+
+        return securityDataSource;
+    }
+
+    public int getIntProperty(String stringProperty){
+        return Integer.parseInt(environment.getProperty(stringProperty));
     }
 }
--- SpringSecurityDemo/src/main/java/spring/security/course/config/DemoSecurityConfig.java
+++ SpringSecurityDemo/src/main/java/spring/security/course/config/DemoSecurityConfig.java
@@ -1,24 +1,36 @@
 package spring.security.course.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    /**
+     * Here I used java code authentication
+            @Override
+            protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+               UserBuilder users = User.withDefaultPasswordEncoder();
+               auth.inMemoryAuthentication()
+                       .withUser(users.username("katya").password("katya").roles("EMPLOYEE"))
+                       .withUser(users.username("dima").password("dima").roles("EMPLOYEE","MANAGER"))
+                       .withUser(users.username("masha").password("masha").roles("EMPLOYEE","ADMIN"));
+            }
+     **/
+
+    @Autowired
+    private DataSource securityDataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-       UserBuilder users = User.withDefaultPasswordEncoder();
-       auth.inMemoryAuthentication()
-               .withUser(users.username("katya").password("katya").roles("EMPLOYEE"))
-               .withUser(users.username("dima").password("dima").roles("EMPLOYEE","MANAGER"))
-               .withUser(users.username("masha").password("masha").roles("EMPLOYEE","ADMIN"));
+        auth.jdbcAuthentication().dataSource(securityDataSource);
     }
 
     @Override

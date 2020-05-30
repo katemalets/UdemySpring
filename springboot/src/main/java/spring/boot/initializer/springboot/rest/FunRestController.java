1a525701663216a9f13a4259132c46a05757a995
katemalets <katyamask@gmail.com>
2020-05-30 10:26:38 +0300

    developing a REST API controller

---
+++ springboot/src/main/java/spring/boot/initializer/springboot/rest/FunRestController.java
@@ -0,0 +1,15 @@
+package spring.boot.initializer.springboot.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+public class FunRestController {
+
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello World! Time on the server : " + LocalDateTime.now();
+    }
+}

304cf54d5ce670a60b8d59ce7552c7f539084935
katemalets <katyamask@gmail.com>
2020-05-31 19:22:05 +0300

    create thymeleaf app with hello controller

---
+++ ThymeleafDemo/src/main/java/thymeleaf/spring/boot/thymeleafdemo/controller/DemoController.java
@@ -0,0 +1,15 @@
+package thymeleaf.spring.boot.thymeleafdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+
+    @GetMapping("/hello")
+    public String sayHello(Model model){
+        model.addAttribute("data", new java.util.Date());
+        return "helloworld";
+    }
+}
---
+++ ThymeleafDemo/src/test/java/thymeleaf/spring/boot/thymeleafdemo/ThymeleafdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package thymeleaf.spring.boot.thymeleafdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

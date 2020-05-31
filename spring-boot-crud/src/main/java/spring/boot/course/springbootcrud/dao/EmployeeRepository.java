8ada348a26c2063db3926a79e941dca07aab6ddb
katemalets <katyamask@gmail.com>
2020-05-31 14:55:11 +0300

    configuration, pagination and sorting

--- spring-boot-crud/src/main/java/spring/boot/course/springbootcrud/dao/EmployeeRepository.java
+++ spring-boot-crud/src/main/java/spring/boot/course/springbootcrud/dao/EmployeeRepository.java
@@ -1,7 +1,9 @@
 package spring.boot.course.springbootcrud.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import spring.boot.course.springbootcrud.entity.Employee;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }

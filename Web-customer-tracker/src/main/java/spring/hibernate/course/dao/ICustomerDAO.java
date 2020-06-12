d8f6a322458399914176647d458ef2c442b8d7c0
katemalets <katyamask@gmail.com>
2020-06-12 12:08:10 +0300

    delete

--- Web-customer-tracker/src/main/java/spring/hibernate/course/dao/ICustomerDAO.java
+++
@@ -1,9 +0,0 @@
-package spring.hibernate.course.dao;
-
-import spring.hibernate.course.entities.Customer;
-
-import java.util.List;
-
-public interface ICustomerDAO {
-    List<Customer> getCustomers();
-}
--- Web-customer-tracker/src/main/java/spring/hibernate/course/service/ICustomerService.java
+++
@@ -1,9 +0,0 @@
-package spring.hibernate.course.service;
-
-import spring.hibernate.course.entities.Customer;
-
-import java.util.List;
-
-public interface ICustomerService {
-    List<Customer> getCustomers();
-}
